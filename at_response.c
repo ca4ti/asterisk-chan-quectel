d0cddb20d68b1d9870a0456e76c0bbe423f2f4c1
IchthysMaranatha <94102122+IchthysMaranatha@users.noreply.github.com>
2021-12-01 11:09:50 +0530

    No force registration status
    Due to CREG parse issues, earlier registration status was always kept on, now will show unregistered if not connected to network

--- at_response.c
+++ at_response.c
@@ -1859,7 +1859,7 @@ static int at_response_creg (struct pvt* pvt, char* str, size_t len)
 	}
 	else
 	{
-		pvt->gsm_registered = 1;
+		pvt->gsm_registered = 0;
 		manager_event_device_status(PVT_ID(pvt), "Unregister");
 	}
 
